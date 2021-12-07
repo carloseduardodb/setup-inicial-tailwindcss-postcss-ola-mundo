ed42a1c65b1a436978b91f48f29e79113a18f324
carloseduardodb <carloseduardodiasbatista@gmail.com>
2021-12-07 19:42:55 -0300

    end project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "p-orange": {
+          "primary": "#FF6433",
+        },
+        "p-green": {
+          "primary": "#00D289",
+        },
+      }
+    },
   },
   variants: {
     extend: {},
